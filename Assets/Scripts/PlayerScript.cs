7c00a833545ad31b41eaba6163162edf64afacb7
lolknight123 <LWaltersdorff@knights.ucf.edu>
2020-10-25 16:01:53 -0400

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text score;
+    public Text win;
+    public Text life;
+    
+
+    private int scoreValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = "Score: " + scoreValue.ToString();
+        win.text = "";
+        // need to do more research on how to make an effective life system before adding enemies 
+        life.text = "Lives: 3";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Money")
+        {
+            scoreValue += 1;
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (scoreValue >= 4)
+        {
+         win.text = "You Win! Game by Logan Waltersdorff   PRESS ESC TO QUIT";
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // this works but i need to find out how to limit the height and how to add more gravity when it reaches that height
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Playercontroller : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    Animator anim;
+
+    void Start()
+
+{
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
+     if (Input.GetKeyDown(KeyCode.D))
+
+        {
+
+          musicSource.clip = musicClipOne;
+
+          musicSource.Play();
+
+          anim.SetInteger("State", 1);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.D))
+
+        {
+
+          musicSource.Stop();
+
+          anim.SetInteger("State", 0);
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.W))
+
+        {
+
+          musicSource.clip = musicClipTwo;
+
+          musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+
+        {
+
+          musicSource.Stop();
+
+          anim.SetInteger("State", 0);
+
+         }
+     if (Input.GetKeyDown(KeyCode.A))
+
+        {
+
+          musicSource.clip = musicClipOne;
+
+          musicSource.Play();
+
+          anim.SetInteger("State", 1);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.A))
+
+        {
+
+          musicSource.Stop();
+
+          anim.SetInteger("State", 0);
+
+         }
+     if (Input.GetKeyDown(KeyCode.L))
+
+        {
+
+          musicSource.loop = true;
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+
+        {
+
+          musicSource.loop = false;
+
+        }
+      
+      if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+          Application.Quit();
+        }
+      
+
+    }
+  public bool facingRight = true; //Flip
+ 
+void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(h > 0 && !facingRight)
+            Flip();
+        else if(h < 0 && facingRight)
+            Flip();
+     }
+void Flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
